34dae1c1bd000a2ae8e3491a267285f855b8e026
JOY <jypjoy@gmail.com>
2023-04-30 15:00:46 +0900

    💄 style: firebase 관련 파일 등록

---
+++ WhatsUp/WhatsUpApp.swift
@@ -0,0 +1,29 @@
+//
+//  WhatsUpApp.swift
+//  WhatsUp
+//
+//  Created by Joy on 2023/04/30.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
+
+@main
+struct WhatsUpApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
